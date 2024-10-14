4de7854e3cc3508e79aa16d8ca3409f6b84dcc7b
Steven Sousa <silver.steven67@gmail.com>
2024-10-14 07:22:47 -0400

    migrated ongoing project to new repository

---
+++ seeds/index.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const axios = require('axios');
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
+
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+
+    // Fetch photos from Unsplash API
+    const photosResponse = await axios.get('https://api.unsplash.com/collections/496276/photos', {
+        params: {
+            client_id: process.env.UNSPLASH_API_KEY,
+            per_page: 50 // Fetch 30 photos in a single request (adjust as necessary)
+        }
+    });
+
+    const photos = photosResponse.data;
+
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const randomPhoto = photos[Math.floor(Math.random() * photos.length)];
+
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: randomPhoto.urls.regular,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quae.',
+            price: Math.floor(Math.random() * 20) + 10
+        });
+
+        await camp.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
