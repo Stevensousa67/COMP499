0719e3bb100a0544114054baacff6dbd1b47d523
Steven Sousa <silver.steven67@gmail.com>
2024-11-05 16:12:59 -0500

    broke out campground routes to clean app.js

---
+++ routes/reviews.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const Campground = require('../models/campground');
+const AppError = require('../utils/AppError');
+const catchAsync = require('../utils/catchAsync');
+const { validateReview } = require('../utils/JOI-Validations.js');
+const Review = require('../models/review');
+const router = express.Router({ mergeParams: true });
+
+router.post('/', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
+module.exports = router;
