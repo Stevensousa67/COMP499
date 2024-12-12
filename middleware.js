c77121050f3eb0b0e0af7605eaf93fcff4da07fd
Steven Sousa <silver.steven67@gmail.com>
2024-12-12 17:51:22 -0500

    added JOI-validation.js code

--- middleware.js
+++ middleware.js
@@ -1,5 +1,44 @@
 const Campground = require('./models/campground');
 const Review = require('./models/review');
+const Joi = require('joi');
+const AppError = require('./utils/AppError');
+
+const validateCampground = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+});
+
+const validateReview = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+});
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = validateCampground.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new AppError(msg, 400);
+    } else {
+        next();
+    };
+};
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = validateReview.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new AppError(msg, 400);
+    } else {
+        next();
+    };
+}
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
