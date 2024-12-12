6486e877fd2cc675bdac0b600afddfb83b18b2f9
Steven Sousa <silver.steven67@gmail.com>
2024-12-12 15:43:59 -0500

    created isAuthor middleware to verify if signed in user is the author of viewing campground.

--- middleware.js
+++ middleware.js
@@ -14,3 +14,12 @@ module.exports.storeReturnTo = (req, res, next) => {
     next();
 }
 
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
